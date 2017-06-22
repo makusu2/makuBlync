7107dd273ac9ebc2fc9374665b3ed1b260008c16
Pitts <pittss@wit.edu>
2017-06-22 07:53:20 -0400

    First push

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BlyncLightTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form = new Form1();
+            Application.Run(form);
+            while (true)
+            {
+                form.BlankTicket = !form.BlankTicket;
+            }
+        }
+    }
+}
